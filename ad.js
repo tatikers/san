465a1a0034a467e8fdcd68c86e9fb87e43c39213
tatikers <91671910+tatikers@users.noreply.github.com>
2021-10-09 20:34:21 +0700

    Update ad.js

--- ad.js
+++ ad.js
@@ -1,1 +1,1 @@
-(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4577009,document.body||document.documentElement)
+(function(s,u,z,p){s.src=u,s.setAttribute("data-zone",z),p.appendChild(s);})(document.createElement("script"),"https://iclickcdn.com/tag.min.js",4577009,document.body||document.documentElement)
