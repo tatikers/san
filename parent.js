e1a657078ac59aaf69926e63ea4bc5cf0218661e
tatikers <91671910+tatikers@users.noreply.github.com>
2021-10-11 10:50:25 +0700

    Update parent.js

--- parent.js
+++ parent.js
@@ -1,1 +1,1 @@
-(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4576693,document.body||document.documentElement)
+(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4206804,document.body||document.documentElement)
