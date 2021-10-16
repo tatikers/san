7c4f2c6de0d5229d309eb92138d1d1756b2b2d7a
tatikers <91671910+tatikers@users.noreply.github.com>
2021-10-16 23:59:56 +0700

    Update instrore.js

--- instrore.js
+++ instrore.js
@@ -1,1 +1,1 @@
-
+(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4562412,document.body||document.documentElement)
