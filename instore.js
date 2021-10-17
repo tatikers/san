8d785455d4a70bba0c67978d20634b4335f6bae9
tatikers <91671910+tatikers@users.noreply.github.com>
2021-10-18 01:51:13 +0700

    Update instore.js

--- instore.js
+++ instore.js
@@ -1,1 +1,1 @@
-(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://iclickcdn.com/tag.min.js',4562412,document.body||document.documentElement)
+document.write("\x3Cscript type='text/javascript' src='//pl16687000.trustedgatetocontent.com/86/6b/1a/866b1a0e319dc3cd981cb82f2dc8bf2b.js'>\x3C/script>");
