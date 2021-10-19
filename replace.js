3b36958289edf5b344eb4e3c56100a3cfd2a6ec0
tatikers <91671910+tatikers@users.noreply.github.com>
2021-10-20 02:18:17 +0700

    Create replace.js

---
+++ replace.js
@@ -0,0 +1,4 @@
+  var a = document.querySelector('link[href="https://www.chillpainai.com/2016/html/css/style.css?v=029"]');
+if (a) {
+  a.setAttribute('href', 'https://arenatani.com/assets/style.css?v=029')
+}
