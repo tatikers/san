904f6273cde12ac7864e9ef5a60b278af2eafd31
tatikers <91671910+tatikers@users.noreply.github.com>
2022-09-07 04:19:24 +0700

    Update tunwalai.js

--- tunwalai.js
+++ tunwalai.js
@@ -1,1 +1,2 @@
 
+window.onload=function(){var o=Math.round(+new Date/1e3);if(console.log("T."+localStorage.puTS+"/"+o),void 0===localStorage.puTS||parseInt(localStorage.puTS)<=o-3600)for(var t=document.getElementsByTagName("a"),e=0,a=t.length;e<a;e++)t[e].onclick=function(t){var e=this.getAttribute("href"),a=this.getAttribute("target");"#"!==e&&"javascript:void(0)"!==e&&(t.preventDefault(),"_blank"==a&&window.open(window.location.href),window.open("https://thaudray.com/4/5363689"),window.location.href=e,localStorage.puTS=o)}};
