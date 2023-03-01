2d868dee7e47875467a2d3faf1a3c77d54ccd232
tatikers <91671910+tatikers@users.noreply.github.com>
2023-03-02 04:59:05 +0700

    Create bloggang.js

---
+++ bloggang.js
@@ -0,0 +1,3 @@
+if(screen.width<500||navigator.userAgent.match(/Android/i)||navigator.userAgent.match(/webOS/i)||navigator.userAgent.match(/iPhone/i)||navigator.userAgent.match(/iPod/i)){
+(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://inklinkor.com/tag.min.js',5171220,document.body||document.documentElement)
+}
