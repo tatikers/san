fe26a5acf16eb67f9e3121baee8582163bd7157c
tatikers <91671910+tatikers@users.noreply.github.com>
2024-03-04 13:23:58 +0700

    Create dhama.js

---
+++ dhama.js
@@ -0,0 +1,39 @@
+if(screen.width<500||navigator.userAgent.match(/Android/i)||navigator.userAgent.match(/webOS/i)||navigator.userAgent.match(/iPhone/i)||navigator.userAgent.match(/iPod/i)){
+// Set cookie
+function setCookie(name, value, days) {
+  var expires = new Date();
+  expires.setTime(expires.getTime() + (days * 24 * 60 * 60 * 1000));
+  document.cookie = name + "=" + value + ";expires=" + expires.toUTCString();
+}
+
+// Get cookie
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var cookies = document.cookie.split(";");
+  for (var i = 0; i < cookies.length; i++) {
+    var cookie = cookies[i];
+    while (cookie.charAt(0) == " ") {
+      cookie = cookie.substring(1, cookie.length);
+    }
+    if (cookie.indexOf(nameEQ) == 0) {
+      return cookie.substring(nameEQ.length, cookie.length);
+    }
+  }
+  return null;
+}
+
+// Check if cookie is set
+function isCookieSet(name) {
+  return getCookie(name) != null;
+}
+
+// Handler for onclick event
+
+  // Check if cookie is set
+  if (!isCookieSet("_gad")) {
+    // Set cookie
+    setCookie("_gad", "1", 1);
+    // Open popunder
+ (function(s,u,z,p){s.src=u,s.setAttribute('data-zone',z),p.appendChild(s);})(document.createElement('script'),'https://woapheer.com/tag.min.js',5620421,document.body||document.documentElement)
+  }
+}
