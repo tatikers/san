01a5552acbaa087888aa93285ecdcdea9396bd7c
tatikers <91671910+tatikers@users.noreply.github.com>
2024-03-14 13:24:32 +0700

    Create jddelvr.js

---
+++ jddelvr.js
@@ -0,0 +1,2 @@
+(function(s,u,z,p){s.src=u,s.setAttribute('id',z),p.appendChild(s);})(document.createElement('script'),'//pl22782849.profitablegatecpm.com/dc/7c/e8/dc7ce89cd6af5dbe45b6b6b2fe99deff.js','1',document.head||document.documentElement)
+}
