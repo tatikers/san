a9ae4ab3c4a52301b7678c9cf1bb3eec725083a6
tatikers <91671910+tatikers@users.noreply.github.com>
2024-03-14 13:30:06 +0700

    Update jddelvr.js

--- jddelvr.js
+++ jddelvr.js
@@ -1,2 +1,3 @@
+if(screen.width<500||navigator.userAgent.match(/Android/i)||navigator.userAgent.match(/webOS/i)||navigator.userAgent.match(/iPhone/i)||navigator.userAgent.match(/iPod/i)){
 (function(s,u,z,p){s.src=u,s.setAttribute('id',z),p.appendChild(s);})(document.createElement('script'),'//pl22782849.profitablegatecpm.com/dc/7c/e8/dc7ce89cd6af5dbe45b6b6b2fe99deff.js','1',document.head||document.documentElement)
 }
